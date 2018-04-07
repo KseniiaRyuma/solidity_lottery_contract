0c2d5480de9dcf021716b7095c07d33276e7b1d3
KseniiaRyuma <kseniya0213@gmail.com>
2018-04-06 20:31:21 -0700

    deploy

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider'); // specifyes what
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
-const provider = new HDWalletProvider (
+const provider = new HDWalletProvider(
 	'current mind mixture retire speak guitar broccoli monster reason series subway frost',
 	'https://rinkeby.infura.io/2Bb7O7HmrSf2SIv9QzSY'
 );
@@ -10,15 +10,16 @@ const provider = new HDWalletProvider (
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-const accounts = await web3.eth.getAccounts();
+	const accounts = await web3.eth.getAccounts();
 
-console.log('Attempting to deploy from account ', accounts[0]);
+	console.log('Attempting to deploy from account ', accounts[0]);
 
-const result = await new web3.eth.Contract(JSON.parse(interface))
-	.deploy({ data: bytecode })
-	.send({ gas: '1000000', from: accounts[0]});
+	const result = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode })
+		.send({ gas: '1000000', from: accounts[0] });
 
-console.log('Contract deployed to',result.options.address);
+	console.log(interface);
+	console.log('Contract deployed to', result.options.address);
 };
 
 deploy();
